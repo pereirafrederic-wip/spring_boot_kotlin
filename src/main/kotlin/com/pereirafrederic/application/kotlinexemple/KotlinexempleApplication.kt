9d758d1ff2db738a755df6d59b66e25686bcbd45
frpereira <frederic-michel.pereira@softeam.fr>
2019-07-17 10:03:06 +0200

    init

---
+++ src/main/kotlin/com/pereirafrederic/application/kotlinexemple/KotlinexempleApplication.kt
@@ -0,0 +1,11 @@
+package com.pereirafrederic.application.kotlinexemple
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinexempleApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinexempleApplication>(*args)
+}
---
+++ src/test/kotlin/com/pereirafrederic/application/kotlinexemple/KotlinexempleApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.pereirafrederic.application.kotlinexemple
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinexempleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
